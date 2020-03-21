2f57a7614e5703bc44ad740bc10ad23e05469a47
pachr16 <32641694+pachr16@users.noreply.github.com>
2020-03-21 15:41:02 +0100

    added user = null and login-check in main activity

--- ShoppingListApp/app/src/main/java/sdu/shoppinglistapp/activities/MainActivity.java
+++ ShoppingListApp/app/src/main/java/sdu/shoppinglistapp/activities/MainActivity.java
@@ -1,16 +1,26 @@
 package sdu.shoppinglistapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import sdu.shoppinglistapp.R;
+import sdu.shoppinglistapp.business.User;
 
 public class MainActivity extends AppCompatActivity {
+    private User user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (user.equals(null)) {
+            // send to login activity
+        } else {
+            // send to shoppingList activity
+        }
     }
 }

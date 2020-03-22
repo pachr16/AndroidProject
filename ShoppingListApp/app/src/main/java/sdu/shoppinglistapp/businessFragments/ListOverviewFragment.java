8ff800b78b7c600ebbf65905a14e842894b9fadf
pachr16 <32641694+pachr16@users.noreply.github.com>
2020-03-22 11:32:17 +0100

    removed unnecessary import

--- ShoppingListApp/app/src/main/java/sdu/shoppinglistapp/businessFragments/ListOverviewFragment.java
+++ ShoppingListApp/app/src/main/java/sdu/shoppinglistapp/businessFragments/ListOverviewFragment.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Debug;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;

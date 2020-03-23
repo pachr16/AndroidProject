1402a22b8c75701453c29f907cfee872965a6dfb
pachr16 <32641694+pachr16@users.noreply.github.com>
2020-03-23 23:29:56 +0100

    basic setup of firebase + tested

--- ShoppingListApp/app/src/main/java/sdu/shoppinglistapp/activities/MainActivity.java
+++ ShoppingListApp/app/src/main/java/sdu/shoppinglistapp/activities/MainActivity.java
@@ -1,9 +1,16 @@
 package sdu.shoppinglistapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,12 +22,29 @@ import sdu.shoppinglistapp.business.User;
 
 public class MainActivity extends AppCompatActivity {
     private User user = null;
+    private FirebaseFirestore fdb = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fdb.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot doc: task.getResult()) {
+                                Log.d("***DEBUG", "onComplete: Succeeded in GET. Got this id: " + doc.getId() + ", with: " + doc.getData());
+                            }
+                        } else {
+                            Log.d("***DEBUG", "onComplete: Failed in GET");
+                        }
+                    }
+                });
+
+        /*
         // FOR TESTING PURPOSES:
         ArrayList<ShopList> slist = new ArrayList<>();
         ArrayList<ShopItem> ilist = new ArrayList<>();
@@ -29,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
         map.put(0, "ThisisaScreenName");
         slist.add(new ShopList(0, "thisisalist", 109850923, ilist, map));
         user = new User("Patrick", "email", 0, slist);
+
+         */
 
         // END OF TESTING PURPOSES
 
@@ -40,10 +66,12 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         // checks if the user is logged in (i.e. not null), and sends them to login if no User object is found. Otherwise redirects to the shoppingActivity.
-        if (user.equals(null)) {
+        if (user == null) {
+            Log.d("***DEBUG", "onResume: User was NULL, going to login activity");
             Intent logIntent = new Intent(this, LoginActivity.class);
             startActivity(logIntent);
         } else {
+            Log.d("***DEBUG", "onResume: User was: " + user.getName() + ", ref: " + user);
             Intent shopIntent = new Intent(this, ShoppingActivity.class);
             shopIntent.putExtra("User", user);
             startActivity(shopIntent);

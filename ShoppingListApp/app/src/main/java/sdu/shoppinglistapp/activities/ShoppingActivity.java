cce76442d4555a06fe7b5c4438140527ea69d1c5
CasaRol <alexander-rol@hotmail.com>
2020-05-10 18:15:11 +0200

    Fragments has been implemented using FragmentStatePagerAdapter
    Should work but needs content for the fragments themself

--- ShoppingListApp/app/src/main/java/sdu/shoppinglistapp/activities/ShoppingActivity.java
+++ ShoppingListApp/app/src/main/java/sdu/shoppinglistapp/activities/ShoppingActivity.java
@@ -1,133 +1,76 @@
 package sdu.shoppinglistapp.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import sdu.shoppinglistapp.R;
-import sdu.shoppinglistapp.business.User;
-import sdu.shoppinglistapp.persistence.DbHandler;
+import sdu.shoppinglistapp.business.SectionsStatePagerAdapter;
+
+//All used fragments
+import sdu.shoppinglistapp.businessFragments.FragmentShoppingListContent;
+import sdu.shoppinglistapp.businessFragments.FragmentShoppingListOverview;
+import sdu.shoppinglistapp.businessFragments.FragmentShoppingMain;
+
 
 public class ShoppingActivity extends AppCompatActivity {
-
-    private TextView welcome;
-    private EditText createList;
-    private Button btn_createList;
-    private Button btn_viewAllLists;
 
     private FirebaseDatabase mFirebaseDatabase;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseReference myRef;
-    private String userId;
 
+    private SectionsStatePagerAdapter mSectionStatePagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping);
 
-        createList = findViewById(R.id.shopping_createList);
-        btn_createList = findViewById(R.id.btn_shopping_createList);
-        btn_viewAllLists = findViewById(R.id.btn_shopping_viewLists);
+        mSectionStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
 
-        mAuth = FirebaseAuth.getInstance();
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
-        userId = user.getUid();
-        myRef = mFirebaseDatabase.getReference().child("users").child(userId);
+        mViewPager = findViewById(R.id.container);
 
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if(user != null) {
-                    Toast.makeText(ShoppingActivity.this, "Signed in", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(ShoppingActivity.this, "Not signed in", Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
-
-        welcome = findViewById(R.id.shopping_welcome);
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                String screenName = dataSnapshot.child("screen_name").getValue().toString();
-                welcome.setText("Welcome " + screenName);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.d("test", "onCancelled: id was: " + userId);
-                Log.w("test", "Failed to read value.", error.toException());
-            }
-        });
-
-        btn_createList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(createList.getText().toString() != null || !createList.getText().toString().equals("")) {
-                    createNewList();
-                } else {
-                    Toast.makeText(ShoppingActivity.this, "Please enter a name for the Shopping list", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
+        setupViewPager(mViewPager);
     }
 
-    private void createNewList() {
-        String listName = (mAuth.getCurrentUser().getUid() + "_" + createList.getText().toString());
+    private void setupViewPager(ViewPager viewPager){
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FragmentShoppingMain(), "Main shopping fragment");
+        adapter.addFragment(new FragmentShoppingListOverview(), "List overview");
+        adapter.addFragment(new FragmentShoppingListContent(), "List Content");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber) {
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
+    public void createNewList(String listName) {
         Map<String, Object> taskMap = new HashMap<>();
 
         myRef = mFirebaseDatabase.getReference("shoppingLists/" + listName);
 
-        myRef.child("listName").setValue(createList.getText().toString());
+        myRef.child("listName").setValue(listName);
 
-        myRef = mFirebaseDatabase.getReference("shoppingLists/" + mAuth.getCurrentUser().getUid() + "_" + createList.getText().toString() + "/subscribers");
+        myRef = mFirebaseDatabase.getReference("shoppingLists/" + mAuth.getCurrentUser().getUid() + "_" + listName + "/subscribers");
         myRef.push().setValue(mAuth.getCurrentUser().getUid());
 
         myRef = mFirebaseDatabase.getReference("users/" + mAuth.getCurrentUser().getUid() + "/subscribed_to");
         myRef.child(listName).setValue(listName);
 
         Toast.makeText(ShoppingActivity.this, "List created", Toast.LENGTH_SHORT).show();
-        createList.setText("");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
-    @Override
-    public void onStop(){
-        super.onStop();
-        if(mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
     }
 }
---
+++ ShoppingListApp/app/src/main/java/sdu/shoppinglistapp/businessFragments/FragmentShoppingMain.java
@@ -0,0 +1,55 @@
+package sdu.shoppinglistapp.businessFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import sdu.shoppinglistapp.R;
+import sdu.shoppinglistapp.activities.ShoppingActivity;
+
+public class FragmentShoppingMain extends Fragment {
+
+    TextView welcome;
+    EditText listName;
+    Button btnCreateList;
+    Button btnViewLists;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_shopping_main, container, false);
+
+        welcome = view.findViewById(R.id.shopping_welcome);
+        listName = view.findViewById(R.id.shopping_createList);
+        btnCreateList = view.findViewById(R.id.btn_shopping_createList);
+        btnViewLists = view.findViewById(R.id.btn_shopping_viewLists);
+
+        btnCreateList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ShoppingActivity)getActivity()).createNewList(listName.getText().toString());
+                listName.setText("");
+                Toast.makeText(getActivity(), "List created", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnViewLists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ShoppingActivity)getActivity()).setViewPager(1);
+            }
+        });
+
+
+        return view;
+    }
+}
